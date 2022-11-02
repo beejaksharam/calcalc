6da03cd0ea03f3bddd68a4146e6ddb50601704e8
Sureshkumar Munuswamy <13384757+beejaksharam@users.noreply.github.com>
2022-11-01 21:29:49 -0400

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-green-50 h-screen">
+      <h1 className="text-4xl text-center justify-center font-extrabold">
+        Calorie Calculator
+      </h1>
+    </div>
+  );
+}
